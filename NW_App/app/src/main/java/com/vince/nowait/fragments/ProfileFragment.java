b341a1fc04a65acec1a175b6bb6d9f7578ec8c61
TERRY <taekim97@gmail.com>
2016-08-11 16:56:51 -0400

    User Icon for profile

--- NW_App/app/src/main/java/com/vince/nowait/fragments/ProfileFragment.java
+++ NW_App/app/src/main/java/com/vince/nowait/fragments/ProfileFragment.java
@@ -1,12 +1,14 @@
 package com.vince.nowait.fragments;
 
 import android.app.Fragment;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.vince.nowait.R;
 
@@ -17,8 +19,13 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         Log.i("Profile Fragment", "ONCREATE PROFILE FRAGMENT");
+        Resources resources = getResources();
 
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        ImageView image = (ImageView) view.findViewById(R.id.profileImageBig);
+        image.setImageResource(R.drawable.profile_icon);
+        return view;
     }
 }
 
