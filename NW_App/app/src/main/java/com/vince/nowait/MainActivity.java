e61f32d526fc0b211e194796a71a40efc7b539d8
vin01188 <vinkang97@gmail.com>
2016-08-13 14:12:52 -0400

    Added API integration

--- NW_App/app/src/main/java/com/vince/nowait/MainActivity.java
+++ NW_App/app/src/main/java/com/vince/nowait/MainActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 
 import com.vince.nowait.fragments.MainFragment;
 import com.vince.nowait.fragments.ProfileFragment;
